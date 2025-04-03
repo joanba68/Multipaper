2868bd9e99885a25623427b638d83842d415f008
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2025-04-03 16:30:37 +0000

    Update constructor

--- MultiPaper-Master/src/main/java/puregero/multipaper/server/velocity/scaling/strategy/None.java
+++ MultiPaper-Master/src/main/java/puregero/multipaper/server/velocity/scaling/strategy/None.java
@@ -3,7 +3,7 @@ package puregero.multipaper.server.velocity.scaling.strategy;
 import java.util.concurrent.TimeUnit;
 
 public class None extends Base {
-    public None() {
+    public None(long interval, TimeUnit timeUnit) {
         super(Long.MAX_VALUE, TimeUnit.DAYS);
     }
 }
