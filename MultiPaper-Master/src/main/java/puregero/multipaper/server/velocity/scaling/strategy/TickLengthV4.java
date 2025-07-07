b60bf972570a1bb54ad7ee57fbe04649403640f2
joanba68 <joan.altadill.elias@gmail.com>
2025-07-07 14:35:06 +0200

    TickLengthv4 improvement to avoid flip-flops

--- MultiPaper-Master/src/main/java/puregero/multipaper/server/velocity/scaling/strategy/TickLengthV4.java
+++ MultiPaper-Master/src/main/java/puregero/multipaper/server/velocity/scaling/strategy/TickLengthV4.java
@@ -18,31 +18,21 @@ import puregero.multipaper.server.velocity.metric.Metrics;
 
 public class TickLengthV4 extends BaseStrategy {
 
-    private static final int DEFAULT_MSPT_HIGH = 40;
-    private static final int DEFAULT_MSPT_LOW = 10;
     private static final double DEFAULT_RED_RATIO = 0.6;
     private static final long DEFAULT_INTERVAL = 2;
     private static final String DEFAULT_UNIT_TIME = "MINUTES";
-    //private static final long DEFAULT_WAITT = 10;
     private static final double DEFAULT_SCALEUP_RATIO = 0.3;
     private static final double DEFAULT_SCALEDOWN_RATIO = 0.3;
     private static final int DEFAULT_MIN_SERVERS_DOWN = 2;
     private static final int DEFAULT_MAX_SERVERS_UP = 10;
-    private static final int DEFAULT_IDEAL_PLAYERS = 40;
 
-    private int msptHigh;
-    private int msptLow;
     private double scaleUpRatio;
     private boolean scalingUp;
     private double scaleDownRatio;
     private boolean scalingDown;
-    //private int waitT;
-    //private int waitScaling;
     private double red;
     private int minServers;
     private int maxServers;
-    private double timeW;
-    private double playerW;
 
     private MetricReporter metrics;
 
@@ -53,24 +43,18 @@ public class TickLengthV4 extends BaseStrategy {
     @Override
     public void onStartup(MultiPaperVelocity plugin) {
         super.onStartup(plugin);
-        this.msptHigh     = Math.toIntExact(config.getLong("performance.tick_length.high", (long) DEFAULT_MSPT_HIGH));
-        this.msptLow      = Math.toIntExact(config.getLong("performance.tick_length.low", (long) DEFAULT_MSPT_LOW));
+
         this.red          = config.getDouble("performance.tick_length.redL", DEFAULT_RED_RATIO);
 
         this.scaleUpRatio    = config.getDouble("scaling.scaleUpRatio", DEFAULT_SCALEUP_RATIO);
         this.scaleDownRatio  = config.getDouble("scaling.scaleDownRatio", DEFAULT_SCALEDOWN_RATIO);
         this.interval        = config.getLong("scaling.interval", DEFAULT_INTERVAL);
         this.timeUnit        = TimeUnit.valueOf(config.getString("scaling.units", DEFAULT_UNIT_TIME));
-        //this.waitT           = Math.toIntExact(config.getLong("scaling.waitT", (long) DEFAULT_WAITT));
         this.minServers      = Math.toIntExact(config.getLong("scaling.minServers", (long) DEFAULT_MIN_SERVERS_DOWN));
         this.maxServers      = Math.toIntExact(config.getLong("scaling.maxServers", (long) DEFAULT_MAX_SERVERS_UP));
 
-        this.timeW   = config.getDouble("defaults.timeW", 5.0);
-        this.playerW = config.getDouble("defaults.playerW", 1.0);
-
         this.scalingUp   = false;
         this.scalingDown = false;
-        //this.waitScaling = 0;
 
     }
 
@@ -119,6 +103,10 @@ public class TickLengthV4 extends BaseStrategy {
         Map<String, Metrics> metricsMap = metrics.getMetrics().stream()
             .collect(Collectors.toMap(Metrics::getName, Function.identity()));
 
+        // Scaling criteria
+        // scale down server if all servers with quality < qualityT * (1 - scaleDownRatio)
+        // scale up server if enough servers with quality > qualityT * (1 + scaleUpRatio)
+
         Collection<ServerWithData> serversWD = allServers
             .stream()
             .map(server -> {
@@ -136,7 +124,7 @@ public class TickLengthV4 extends BaseStrategy {
                     );
                 }
                 return new ServerWithData(
-                    wmetrics.getQuality() >= qualityT, // Comparar qualitat amb el llindar
+                    wmetrics.getQuality() >= qualityT * (1 + scaleUpRatio), // Comparar qualitat amb el llindar i donar un marge
                     server,
                     wmetrics.getPlayers(),
                     wmetrics.getMspt(),
@@ -146,34 +134,16 @@ public class TickLengthV4 extends BaseStrategy {
             })
             .collect(Collectors.toList());
 
-        // for (ServerWithData serverX : serversWD){
-        //     double mspt = ServerConnection.getConnection(serverX.getServer().getServerInfo().getName()).getTimer().averageInMillis();
-        //     double quality = ServerConnection.getConnection(serverX.getServer().getServerInfo().getName()).getTimer().averageInMillis() * this.timeW + serverX.getPlayers() * this.playerW;
-        //     logger.info("{} {} P {} mspt, {} OWNC {} Q, degraded perf. {}", 
-        //         serverX.getServer().getServerInfo().getName(), 
-        //         serverX.getPlayers(), 
-        //         Math.round(mspt),
-        //         Math.round(serverX.getChunks()),
-        //         Math.round(quality),
-        //         serverX.getPerf());
-        // }
-
         Map<Boolean, List<ServerWithData>> partitionedServers = serversWD
             .stream()
             .collect(Collectors.partitioningBy(server -> server.getPerf()));
-
         ServerWithData[] serversBad = partitionedServers.get(true).toArray(new ServerWithData[0]);
-        //ServerWithData[] serversOk  = partitionedServers.get(false).toArray(new ServerWithData[0]);
-
         long counterBad = serversBad.length;
-        //long counterOk  = serversOk.length;
 
         // Now to consider scale up servers
         logger.info("Servers with degraded tick time: {}", counterBad);
 
         redServers      = (long) Math.round(red * (double) allServers.size());
-        scaleUpServers  = (long) Math.round(scaleUpRatio * (double) counterBad);
-        if (scaleUpServers != 0) logger.info("Required {} degraded servers for scale up", scaleUpServers);
         
         // From here, we have servers with degraded tick time and enough servers to migrate players
         // if too many servers are degraded, no need to migrate, should be scale up from scaling manager
